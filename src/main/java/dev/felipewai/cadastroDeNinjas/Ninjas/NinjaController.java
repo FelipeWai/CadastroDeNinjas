f9fba5882305205a9f009b8a1c367a60aa6845fa
Felipe Wai <felipe.wai@t2s.com.br>
2025-01-15 08:35:19 -0300

    Feat Lombok

--- src/main/java/dev/felipewai/cadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/felipewai/cadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,4 +1,4 @@
-package dev.felipewai.cadastroDeNinjas;
+package dev.felipewai.cadastroDeNinjas.Ninjas;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
---
+++ src/main/java/dev/felipewai/cadastroDeNinjas/Ninjas/NinjaModel.java
@@ -0,0 +1,30 @@
+package dev.felipewai.cadastroDeNinjas.Ninjas;
+
+import dev.felipewai.cadastroDeNinjas.Missoes.MissoesModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    private Integer idade;
+
+    // @ManyToOne um ninja tem uma unica missao
+    @ManyToOne
+    @JoinColumn(name = "missoes_id") //Foreign Key
+    private MissoesModel missoes;
+}
