cfdfcc423b34c27fcb62017a71765304820de852
Felipe Wai <felipe.wai@t2s.com.br>
2025-01-16 14:15:29 -0300

    feat:Adiciona novos endpoints

--- src/main/java/dev/felipewai/cadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/felipewai/cadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package dev.felipewai.cadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,6 +9,36 @@ public class NinjaController {
     @GetMapping("/boasvindas")
     public String boasVindas(){
         return "Essa é minha primeira mensagem nessa rota";
+    }
+
+    //Adicionar ninja (CREATE)
+    @PostMapping("/criar")
+    public String criarNinja(){
+        return "Criado com sucesso!";
+    }
+
+    //Procurar ninja por id (READ)
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinjas(){
+        return "Lista de ninjas";
+    }
+
+    // Mostrar ninja (READ)
+    @GetMapping("/todosID")
+    public String mostrarTodosOsNinjasPorId(){
+        return "Ninja por ID";
+    }
+
+    // Alterar dados do ninja
+    @PutMapping("/alterarID")
+    public String atualizarNinja(){
+        return "Atualizado com sucesso!";
+    }
+
+    // Deletar Ninja
+    @DeleteMapping("/deletarID")
+    public String deletarNinja(){
+        return "Deletado com sucesso!";
     }
 
 }
